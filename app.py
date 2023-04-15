a2ff34cbcd60f4146f7648076838516b86b7e2f5
HMoussa98 <wordpresstest1.0.0@gmail.com>
2023-04-15 17:29:56 +0200

    13-4 v2

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ def index():
      movies = Movie.query.all()
      return render_template('index.html', movies=movies)
 
+@app.route('/films')
+def films():
+     movies = Movie.query.all()
+     return render_template('films.html', movies=movies)
+
+@app.route('/about')
+def about():
+     return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
